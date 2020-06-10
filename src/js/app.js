d1fc393f6e74963d0f56d8b2fe1502d228f7f798
hakantanka <hakantanka@LAPTOP-2BSSM05I.localdomain>
2020-06-10 06:37:32 +0200

    window.body corrected to document.body

--- src/js/app.js
+++ src/js/app.js
@@ -8,11 +8,11 @@ let clickedMap = false;
 svg.addEventListener('click', e => {if(e.target.id.length === 2) loadInfo(e.target);});
 
 // set body height
-window.body.style.height = window.innerHeight;
+document.body.style.height = window.innerHeight;
 
 // set margin left of the map according to viewport width
 window.addEventListener('resize', () => {  
-    window.body.style.height = window.innerHeight; 
+    document.body.style.height = window.innerHeight; 
     if(window.innerWidth > 1600) {svg.style.marginLeft = '0px'}
     else if(window.innerWidth > 1300) {
         if(clickedMap) {
